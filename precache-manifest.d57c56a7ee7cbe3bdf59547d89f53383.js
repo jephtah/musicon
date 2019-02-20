34e401c99e98fc14fc6550ccea58ad709a7dd2e8
Jephtah Uche <jephtino@gmail.com>
2019-02-20 08:18:40 +0100

    Updates

---
+++ precache-manifest.d57c56a7ee7cbe3bdf59547d89f53383.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "289976330eca5492c292",
+    "url": "/musicon/static/css/main.5642b26f.chunk.css"
+  },
+  {
+    "revision": "289976330eca5492c292",
+    "url": "/musicon/static/js/main.28997633.chunk.js"
+  },
+  {
+    "revision": "916645e5d53100d27edc",
+    "url": "/musicon/static/js/runtime~main.916645e5.js"
+  },
+  {
+    "revision": "489eedd4eac56958c7d9",
+    "url": "/musicon/static/js/2.489eedd4.chunk.js"
+  },
+  {
+    "revision": "01aae6d98ce41dcc90f7936b8623ee17",
+    "url": "/musicon/static/media/slide_one.01aae6d9.jpg"
+  },
+  {
+    "revision": "3c10e23dda2f9514556255b994740986",
+    "url": "/musicon/static/media/slide_two.3c10e23d.jpg"
+  },
+  {
+    "revision": "556282e2130884b1358820c9d2df629c",
+    "url": "/musicon/static/media/slide_three.556282e2.jpg"
+  },
+  {
+    "revision": "9ec2ef808a43bb0c027b274219502282",
+    "url": "/musicon/index.html"
+  }
+];
